3f73a0aea31480b2c80c8116957a46ed0af6e642
Nelson Lumpkin <ossocasa@gmail.com>
2023-03-20 14:54:22 -0500

    first commit

---
+++ scripts/Orders.js
@@ -0,0 +1,98 @@
+import { getProducts, getEmployees, getOrders } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [, productId] = itemClicked.id.split("--")
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} costs $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
+const employeeOrders = (employee) => {
+    let fulfilledOrders = 0
+
+    for (const order of orders) {
+        if (order.employeeId === employee.id) {
+            fulfilledOrders += 1
+        }
+    }
+    return fulfilledOrders
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+
+                    const orderCount = employeeOrders(employee)
+
+                    window.alert(`${employee.name} sold ${orderCount} products`)
+                }
+            }
+        }
+    }
+)
+
+
+
+
+
+// Get copy of state for use in this module
+const products = getProducts()
+const employees = getEmployees()
+const orders = getOrders()
+
+
+// Function whose responsibility is to find the product for an order
+const findProduct = (order, products) => {
+    let orderProduct = null
+
+    for (const product of products) {
+        if (product.id === order.productId) {
+            orderProduct = product
+        }
+    }
+
+    return orderProduct
+}
+
+// Function whose responsibility is to find the employee for an order
+const findEmployee = (order, employees) => {
+    let orderEmployee = ""
+
+    for (const employee of employees) {
+        if (employee.id === order.employeeId) {
+            orderEmployee = employee
+        }
+    }
+
+    return orderEmployee
+}
+
+export const Orders = () => {
+    let html = ""
+    html = "<ul>"
+
+    for (const order of orders) {
+        const employee = findEmployee(order, employees)
+        const product = findProduct(order, products)
+
+        html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
